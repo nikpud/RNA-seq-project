a30cdae32dc996c284b568da0766d9334c7af206
Nik Pudaruth <nik.pudaruth@gmail.com>
2025-03-01 23:15:13 +1100

    feat: added comments

--- example.py
+++ example.py
@@ -8,18 +8,24 @@ from pydeseq2.default_inference import DefaultInference
 from pydeseq2.ds import DeseqStats
 from pydeseq2.utils import load_example_data
 
+# Loading synthetic data
 counts_df = load_example_data(
     modality="raw_counts",
     dataset="synthetic",
     debug=False,
 )
 
+# printing synthetic counts dataframe
 print(counts_df)
 
+# loading synthetic metadata for counts dataframe
 metadata = load_example_data(
     modality="metadata",
     dataset="synthetic",
     debug=False,
 )
 
+# printing synthetic metadata for counts dataframe
 print(metadata)
+
+
