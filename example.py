6fe087d88228f70095d422be01c8cde0aac7d4c4
Nik Pudaruth <nik.pudaruth@gmail.com>
2025-03-01 23:09:33 +1100

    fix: fixing conda environment

--- example.py
+++ example.py
@@ -1,2 +1,10 @@
 import os
 import pickle as pkl
+
+import numpy as np
+
+from pydeseq2.dds import DeseqDataSet
+from pydeseq2.default_inference import DefaultInference
+from pydeseq2.ds import DeseqStats
+from pydeseq2.utils import load_example_data
+
